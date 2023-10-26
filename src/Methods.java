2bfe2175a239272ceb8f25872296e41d152da0ec
Nowacki97PL <noszo997@gmail.com>
2023-10-26 13:20:32 +0200

    add first method

---
+++ src/Methods.java
@@ -0,0 +1,22 @@
+package src;
+
+public class Methods {
+    public static void main(String[] args) {
+
+        calculateScore(true, 800, 10, 300);
+        calculateScore(true, 1200, 8, 500);
+
+    }
+
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
+        int finalScore = score;
+
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your Final Score was: " + finalScore);
+        }
+    }
+}
+
--- src/third.java
+++ src/third.java
@@ -8,5 +8,22 @@ public class third {
         System.out.println(myDouble3);
         double theRemainder = myDouble3 % 40.00d;
         System.out.println("The remainder = " + theRemainder );
+        calculateScore();
+    }
+
+    public static void calculateScore() {
+        boolean gameOver = true;
+        int score = 4000;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        int finalScore = score;
+
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your Final Score was: " + finalScore);
+        }
     }
 }
+
